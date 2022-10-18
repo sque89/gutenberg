9aa7de2862a17dc1572b89bf57d579ce6accb809
Daniel Richards <daniel.richards@automattic.com>
2022-10-18 10:01:38 +0800

    Add initial visualizer popover, sized to the full-width

--- packages/block-editor/src/hooks/index.js
+++ packages/block-editor/src/hooks/index.js
@@ -21,6 +21,7 @@ import './content-lock-ui';
 import './metadata';
 import './metadata-name';
 
+export { AlignmentVisualizer } from './align';
 export { useCustomSides } from './dimensions';
 export { useLayoutClasses, useLayoutStyles } from './layout';
 export { getBorderClassesAndStyles, useBorderProps } from './use-border-props';
