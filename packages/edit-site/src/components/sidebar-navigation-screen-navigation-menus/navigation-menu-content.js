6e2518fc98d676e85bf2279643b3552e71836fec
Jorge Costa <jorge.costa@developer.pt>
2023-03-03 13:09:13 +0000

    Update: Hide page list on single page list menus.

--- packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/navigation-menu-content.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/navigation-menu-content.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import {
@@ -8,6 +13,7 @@ import {
 	BlockTools,
 } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
@@ -15,17 +21,51 @@ import { useSelect } from '@wordpress/data';
 import { unlock } from '../../private-apis';
 import { NavigationMenuLoader } from './loader';
 
-export default function NavigationMenuContent( { rootClientId, onSelect } ) {
-	const { clientIdsTree, isLoading } = useSelect(
-		( select ) => {
-			const { __unstableGetClientIdsTree, areInnerBlocksControlled } =
-				select( blockEditorStore );
-			return {
-				clientIdsTree: __unstableGetClientIdsTree( rootClientId ),
+// Needs to be kept in sync with the query used at packages/block-library/src/page-list/edit.js.
+const MAX_PAGE_COUNT = 100;
+const PAGES_QUERY = [
+	'postType',
+	'page',
+	{
+		per_page: MAX_PAGE_COUNT,
+		_fields: [ 'id', 'link', 'menu_order', 'parent', 'title', 'type' ],
+		// TODO: When https://core.trac.wordpress.org/ticket/39037 REST API support for multiple orderby
+		// values is resolved, update 'orderby' to [ 'menu_order', 'post_title' ] to provide a consistent
+		// sort.
+		orderby: 'menu_order',
+		order: 'asc',
+	},
+];
 
+export default function NavigationMenuContent( { rootClientId, onSelect } ) {
+	const { clientIdsTree, isLoading, isSinglePageList } = useSelect(
+		( select ) => {
+			const {
+				__unstableGetClientIdsTree,
+				areInnerBlocksControlled,
+				getBlockName,
+			} = select( blockEditorStore );
+			const { isResolving } = select( coreStore );
+
+			const _clientIdsTree = __unstableGetClientIdsTree( rootClientId );
+			const hasOnlyPageListBlock =
+				_clientIdsTree.length === 1 &&
+				getBlockName( _clientIdsTree[ 0 ].clientId ) ===
+					'core/page-list';
+			const isLoadingPages =
+				hasOnlyPageListBlock &&
+				isResolving( 'getEntityRecords', PAGES_QUERY );
+			return {
+				clientIdsTree: _clientIdsTree,
 				// This is a small hack to wait for the navigation block
 				// to actually load its inner blocks.
-				isLoading: ! areInnerBlocksControlled( rootClientId ),
+				isLoading:
+					! areInnerBlocksControlled( rootClientId ) ||
+					isLoadingPages,
+				isSinglePageList:
+					hasOnlyPageListBlock &&
+					! isLoadingPages &&
+					_clientIdsTree[ 0 ].innerBlocks.length > 0,
 			};
 		},
 		[ rootClientId ]
@@ -39,12 +79,23 @@ export default function NavigationMenuContent( { rootClientId, onSelect } ) {
 		<>
 			{ isLoading && <NavigationMenuLoader /> }
 			{ ! isLoading && (
-				<OffCanvasEditor
-					blocks={ clientIdsTree }
-					onSelect={ onSelect }
-					LeafMoreMenu={ LeafMoreMenu }
-					showAppender={ false }
-				/>
+				<div
+					className={ classnames(
+						'edit-site-sidebar-navigation-screen-navigation-menus-off-canvas-container',
+						{ 'is-single-page-list': isSinglePageList }
+					) }
+				>
+					<OffCanvasEditor
+						blocks={
+							isSinglePageList
+								? clientIdsTree[ 0 ].innerBlocks
+								: clientIdsTree
+						}
+						onSelect={ onSelect }
+						LeafMoreMenu={ LeafMoreMenu }
+						showAppender={ false }
+					/>
+				</div>
 			) }
 			<div style={ { display: 'none' } }>
 				<BlockTools>
