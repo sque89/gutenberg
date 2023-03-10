704be77e504a5a791a810549e44b6ddb0e1c3f5e
Daniel Richards <daniel.richards@automattic.com>
2023-03-10 17:15:13 +0800

    Try: hide full and wide alignments at smaller container sizes

--- packages/block-editor/src/components/block-alignment-visualizer/visualization.js
+++ packages/block-editor/src/components/block-alignment-visualizer/visualization.js
@@ -22,12 +22,20 @@ export default function Visualization( {
 		<>
 			<style>
 				{ `
-					.block-editor-alignment-visualizer__visualization {
+					.block-editor-alignment-visualizer__visualization-container {
 						--content-size: ${ contentSize ?? '0px' };
 						--wide-size: ${ wideSize ?? '0px' };
 						--gap: 8px;
 						--wide-segment-width: calc( ((var(--wide-size) - var(--content-size)) / 2) - var(--gap) );
 
+						container: visualization / inline-size;
+						position: absolute;
+						display: block;
+						width: 100%;
+						height: 100%;
+					}
+
+					.block-editor-alignment-visualizer__visualization {
 						position: absolute;
 						width: 100%;
 						height: 100%;
@@ -36,58 +44,103 @@ export default function Visualization( {
 						grid-template-columns: 1fr var(--wide-segment-width) var(--content-size) var(--wide-segment-width) 1fr;
 					}
 
-					.block-editor-alignment-visualizer__visualization.is-content-justification-right {
-						--wide-segment-width: calc( (var(--wide-size) - var(--content-size)) - var(--gap) );
-						grid-template-columns: 1fr var(--wide-segment-width) var(--content-size);
-					}
-
-					.block-editor-alignment-visualizer__visualization.is-content-justification-left {
-						--wide-segment-width: calc( (var(--wide-size) - var(--content-size)) - var(--gap) );
-						grid-template-columns: var(--content-size) var(--wide-segment-width) 1fr;
-					}
-
 					.block-editor-alignment-visualizer__visualization-segment {
 						background: #3d5af2;
 						opacity: 0.2;
 						border-radius: 2px;
 					}
+
+					/* Hide wide width alignments when the container is smaller than wide size */
+					@container visualization (max-width: ${ wideSize ?? '0px' }) {
+						.block-editor-alignment-visualizer__visualization {
+							grid-template-columns: 1fr var(--content-size) 1fr;
+						}
+
+						.block-editor-alignment-visualizer__visualization-segment.wide-width {
+							display: none;
+						}
+					}
+
+					/* Hide both wide and full width alignments when the container is smaller than content size. */
+					@container visualization (max-width:  ${ contentSize ?? '0px' }) {
+						.block-editor-alignment-visualizer__visualization {
+							grid-template-columns: 1fr;
+						}
+
+						.block-editor-alignment-visualizer__visualization-segment.wide-width,
+						.block-editor-alignment-visualizer__visualization-segment.full-width {
+							display: none;
+						}
+					}
+
+					/* Hide right side segments when content justification is right */
+					.block-editor-alignment-visualizer__visualization.is-content-justification-right {
+						--wide-segment-width: calc( (var(--wide-size) - var(--content-size)) - var(--gap) );
+						grid-template-columns: 1fr var(--wide-segment-width) var(--content-size);
+					}
+
+					.block-editor-alignment-visualizer__visualization.is-content-justification-right .right-side {
+						display: none;
+					}
+
+					/* Hide left side segments when content justification is left */
+					.block-editor-alignment-visualizer__visualization.is-content-justification-left {
+						--wide-segment-width: calc( (var(--wide-size) - var(--content-size)) - var(--gap) );
+						grid-template-columns: var(--content-size) var(--wide-segment-width) 1fr;
+					}
+
+					.block-editor-alignment-visualizer__visualization.is-content-justification-left .left-side {
+						display: none;
+					}
 				` }
 			</style>
-			<div
-				className={ classnames(
-					'block-editor-alignment-visualizer__visualization',
-					{
-						[ `is-content-justification-${ justification }` ]:
-							justification,
-					}
-				) }
-			>
-				{ justification !== 'left' &&
-					[ ...alignments ]
+			<div className="block-editor-alignment-visualizer__visualization-container">
+				<div
+					className={ classnames(
+						'block-editor-alignment-visualizer__visualization',
+						{
+							[ `is-content-justification-${ justification }` ]:
+								justification,
+						}
+					) }
+				>
+					{ [ ...alignments ]
 						.reverse()
 						.map(
 							( { name } ) =>
 								( name === 'full' || name === 'wide' ) && (
-									<div
+									<VisualizationSegment
 										key={ `${ name }-left` }
-										className={ `block-editor-alignment-visualizer__visualization-segment` }
+										alignment={ name }
+										side="left"
 									/>
 								)
 						) }
-				<div
-					className={ `block-editor-alignment-visualizer__visualization-segment` }
-				/>
-				{ justification !== 'right' &&
-					alignments.map(
+					<VisualizationSegment alignment="content" />
+					{ alignments.map(
 						( { name } ) =>
 							( name === 'full' || name === 'wide' ) && (
-								<div
+								<VisualizationSegment
 									key={ `${ name }-right` }
-									className={ `block-editor-alignment-visualizer__visualization-segment` }
+									alignment={ name }
+									side="right"
 								/>
 							)
 					) }
+				</div>
 			</div>
 		</>
+	);
+}
+
+function VisualizationSegment( { side, alignment } ) {
+	return (
+		<div
+			className={ classnames(
+				'block-editor-alignment-visualizer__visualization-segment',
+				`${ alignment }-width`,
+				{ [ `${ side }-side` ]: side }
+			) }
+		/>
 	);
 }
